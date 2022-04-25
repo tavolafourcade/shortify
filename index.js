f2b22fbc9b707fe1d48df3164d4563095f04bd53
tavolafourcade <tavolafourcade@gmail.com>
2022-04-25 15:20:17 -0500

    [ ADD ] routes and POST request to /api/url/shorten

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ connectDB()
 
 app.use(express.json({extended: false})) //Allow to accept JSON data in API
 
+// Define Routes
+app.use('/', require('./routes/index'))
+app.use('/api/url', require('./routes/url'))
+
 const PORT = 5000
 
 app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`))
